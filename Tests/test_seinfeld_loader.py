4c843d9f92fd8d838b0c4871041e50a144380d5b
Max Logan <155565637+maxnchief@users.noreply.github.com>
2025-12-18 08:45:30 -0600

    Update test_seinfeld_loader.py

--- Tests/test_seinfeld_loader.py
+++ Tests/test_seinfeld_loader.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from app import app
 
 # ----------------------------
-# Fixture for Flask test client
+# Fixture for flask test client
 # ----------------------------
 @pytest.fixture
 def client():
